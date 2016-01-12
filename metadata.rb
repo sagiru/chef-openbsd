92765b80153c32088b35e279e168fa215a25e786
Sascha Girrulat <sascha.girrulat@sysfive.com>
2016-01-12 11:13:16 +0100

    Add metadata.rb to be a more valid chef cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "openbsd"
+maintainer       "Joe Miller"
+maintainer_email "joeym@joeym.net"
+license          "Apache 2.0"
+description      "Includes basic LWRPs for OpenBSD"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
